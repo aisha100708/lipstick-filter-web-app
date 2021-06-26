9372e16f7c2daf2a7b935c82eca940f6a9fb1e73
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-06-26 19:02:40 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+}
+
+function setup() {
+    canvas = createCanvas(640, 480);
+    canvas.center();
+}
+
+function draw() {
+}
+
+function take_snapshot() {
+    save("myImage.png");
+}
