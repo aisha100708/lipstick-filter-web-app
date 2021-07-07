37c0372205f568c423f24b3624ed7b1b409f7b26
aisha100708 <71584539+aisha100708@users.noreply.github.com>
2021-07-07 14:55:01 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,11 +4,34 @@ function preload() {
 function setup() {
     canvas = createCanvas(640, 480);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(640, 480);
+    video.hide();
+    posenet = ml5.poseNet(video, modelLoaded);
+    posenet.on('pose', getPoses);
 }
 
 function draw() {
+    image(video, 0, 0, 640, 480);
 }
 
 function take_snapshot() {
     save("myImage.png");
+}
+
+function modelLoaded() {
+    console.log("PoseNet is Initialized");
+}
+
+function getPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+        nose_x = results[0].pose.nose.x - 25;
+        nose_y = results[0].pose.nose.y - 20;
+        console.log("nose' x position is " + nose_x);
+        console.log("nose' y position is " + nose_y)
+    }
+    else {
+        console.log("Error!")
+    }
 }
